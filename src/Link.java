7207556bf7cb14fc76f471fdb58df3ac97b9663f
Milind Jagota <milind.jagota@gmail.com>
2016-07-03 10:19:52 -0400

    commit

--- src/Link.java
+++ src/Link.java
@@ -4,12 +4,41 @@ public class Link {
 	Node startNode;
 	Node endNode;
 	double length;
+	//added attributes
+	double speed_limit
+	double speed
+	int capacity 
+	// double accident_risk(x,v)
+	boolean has_accident
+	double accident_location 
+
+
 	
 	Link (Node start, Node end, double distance)
 	{
 		startNode = start;
 		endNode = end;
 		length = distance;
+	}
+
+	public double getLength(){
+		return this.length;
+
+	}
+	public double speed_limit(){
+		return this.speed_limit;
+	}
+	public int capacity(){
+		return this.capacity;
+	}
+	public boolean has_accident(){
+		return this.has_accident;
+	}
+
+	public double accident_risk(x,v){
+		//modify this:
+		
+		return 0;
 	}
 	
 }
